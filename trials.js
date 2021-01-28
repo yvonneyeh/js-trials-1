f57eca84e5e7c74da135c8373454b032abcd48f9
Yvonne Yeh <yvonneyeh@me.com>
2021-01-28 21:05:04 +0000

    Translate 7 JavaScript functions to Python

--- trials.js
+++ trials.js
@@ -18,7 +18,7 @@ function outputAllItems(items) {
 //
 // Ex.:
 //   > getAllEvens([7, 8, 10, 1, 2, 2]);
-//   [8, 2, 2]
+//   [8, 10, 2, 2]
 function getAllEvens(nums) {
   const evenNums = [];
 
@@ -82,6 +82,7 @@ function getRange(start, stop) {
   for (let num = start; num < stop; num += 1) {
     nums.push(num);
   }
+  return nums;
 }
 
 
@@ -97,7 +98,9 @@ function censorVowels(word) {
     if ('aeiou'.includes(letter)) {
       chars.push('*');
     }
-    chars.push(letter);
+    else {
+      chars.push(letter);
+    }
   }
 
   return chars.join('');
--- trials.py
+++ trials.py
@@ -2,31 +2,111 @@
 
 
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
+    """"Print each item in the given list
+
+    >>> output_all_items([1, "hello", True])
+    1
+    hello
+    True
+    
+    """
+    for item in items:
+        print(item)
 
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
+    """Given a list of numbers, return a list of all even numbers.
+    
+    >>> get_all_evens([7, 8, 10, 1, 2, 2])
+    [8, 10, 2, 2]
+
+    """
+    even_nums = []
+    for num in nums:
+        if num % 2 == 0:
+            even_nums.append(num)
+    return even_nums
 
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
+    """Given a list, return all elements at the odd number indices.
+
+    >>> get_odd_indices([1, "hello", True, 500])
+    ["hello", 500]
+
+    """
+    result = []
+
+    for idx in range(len(items)):
+        if idx % 2 != 0:
+            result.append(items[idx])
+    return result
 
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+    """
+    Given a list, oput a numbered list.
+
+    >>> print_as_numbered_list([1, "hello", True])
+    1. 1
+    2. hello
+    3. True
+
+    """
+    i = 1
+
+    for item in items:
+        print(f'{i}. {item}')
+        i += 1
 
 
 def get_range(start, stop):
-    pass  # TODO: replace this line with your code
+    """Return a list of numbers in a certain range.
+
+    >>> get_range(0, 5)
+    [0, 1, 2, 3, 4]
+
+    >>> get_range(1, 3)
+    [1, 2]
+    
+    """
+    nums = []
+
+    for num in range(start, stop):
+        nums.append(num)
+    
+    return nums
 
 
 def censor_vowels(word):
-    pass  # TODO: replace this line with your code
+    """ Given a string, return a string where vowels are replaced with "*".
+    
+    >>> censor_vowels("hello world")
+    'h*ll* w*rld'
+    """
+    chars = []
+
+    for letter in word:
+        if letter in "aeiou":
+            chars.append("*")
+        else:
+            chars.append(letter)
+    return "".join(chars)
 
 
 def snake_to_camel(string):
-    pass  # TODO: replace this line with your code
+    """Given a string in snake case, return a string in upper camel case
+
+    >>> snake_to_camel("hello_world")
+    'HelloWorld'
+    
+    """
+    camel_case = []
+
+    for word in string.split("_"):
+        camel_case.append(f"{word[0].upper()}{word[1:]}")
+
+    return "".join(camel_case)
 
 
 def longest_word_length(words):
